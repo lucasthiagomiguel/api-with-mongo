e48bdce9a60827db3b9b1de09d56bb97153cc4aa
lucas.miguel <lucas.thiagomiguel@hotmail.com>
2022-03-13 19:08:30 -0300

    produção

--- src/modules/users/routes/sessions.routes.ts
+++ src/modules/users/routes/sessions.routes.ts
@@ -1,5 +1,4 @@
 import { Router } from "express";
-import { celebrate, Joi, Segments } from 'celebrate';
 import SessionsController from "../controllers/SessionsController";
 
 
@@ -7,13 +6,6 @@ const sessionrRouter = Router();
 const sessionsController = new SessionsController();
 
 sessionrRouter.post(
-    '/',
-    celebrate({
-      [Segments.BODY]: {
-        email: Joi.string().email().required(),
-        senha: Joi.string().required(),
-      },
-    }),
-    sessionsController.create,
+    '/',sessionsController.create,
   );
   export default sessionrRouter;
