60d6bd8f9b011b897bcf206dd6a3533c3934b08d
Lucas de Souza <lsantos@fastprobr.com>
2022-06-01 17:24:51 -0300

    rota cadastrar produtos

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import userRouter from "@modules/users/routes/users.routes";
+import productsRouter from "@modules/produtos/routes/products.routes";
 import sessionrRouter from "@modules/users/routes/sessions.routes";
 import starWarsRouter from "@modules/starWars/routes/star.routes";
 
@@ -8,5 +9,6 @@ const routes = Router();
 routes.use('/users', userRouter);
 routes.use('/login', sessionrRouter);
 routes.use('/star', starWarsRouter);
+routes.use('/products', productsRouter);
 
 export default routes;
